2a44695d804b678be37af3251a6cfad539a60e19
Casey Foster <c@sey.me>
2013-11-09 14:58:14 -0600

    Remove test dummies

--- client/index.js
+++ client/index.js
@@ -116,34 +116,4 @@
   }
 
   drawLoop();
-
-  // /* ====== TEMP: REMOVE WHEN WE HAVE LIVE SOCKET PEOPLE ======== */
-
-  // console.log('stuff', window.NodeKick, app)
-
-  var player1 = new window.NodeKick.Player('bob');
-  player1.x = 70;
-  player1.y = 0;
-  player1.state = 'stand';
-  player1.dir = -1;
-
-  // console.log('the player', player1);
-
-  var player2 = new window.NodeKick.Player('fred');
-  player2.x = 500;
-  player2.y = -300;
-  player2.state = 'jump';
-  player2.dir = 1;
-
-  // app.users[player1.handle] = player1;
-  // app.users[player2.handle] = player2;
-
-  setInterval(function() {
-    player2.x += 10;
-  }, 100);
-
-
-  // /* ============================================================ */
-
-
 })();
