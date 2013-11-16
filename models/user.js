f1ddd67f94f9ce269d032a78c03a4beb47243dc4
Casey Foster <c@sey.me>
2013-11-16 10:32:06 -0600

    Wrap around

--- models/user.js
+++ models/user.js
@@ -55,8 +55,15 @@
           y: this.get('y') + this.get('yv') * dt
         });
       }
-      if (this.get('y') > 0) return;
-      this.set({y: 0, yv: 0, xv: 0, state: 'standing', touchedGround: true});
+      if (this.get('y') <= 0) {
+        this.set({y: 0, yv: 0, xv: 0, state: 'standing', touchedGround: true});
+      }
+      var x = this.get('x');
+      var absX = Math.abs(x);
+      var side = x / absX;
+      if (absX > 1000) {
+        this.set('x', -side * (2 * 1000 - absX));
+      }
     },
 
     moveLeft: function () { this.setDir(-1); },
